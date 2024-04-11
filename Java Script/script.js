03e89fc2d11cbb15c548dfec4c687ba5bf404436
MOKSHITH Y NANDA <codersy17mc@gmail.com>
2024-04-12 04:44:04 +0530

    Update script.js
    Signed-off-by: MOKSHITH Y NANDA <codersy17mc@gmail.com>

--- Java Script/script.js
+++ Java Script/script.js
@@ -1,21 +1,42 @@
-var captcha = document.getElementById("captcha");
-var captchaInput = document.getElementById("captcha-input");
+let captcha;
+function generate() {
 
-function generateCaptcha() {
-  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var captchaText = "";
-  for (var i = 0; i < 6; i++) {
-    captchaText += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  captcha.innerHTML = captchaText;
+	// Clear old input
+	document.getElementById("submit").value = "";
+
+	// Access the element to store
+	// the generated captcha
+	captcha = document.getElementById("image");
+	let uniquechar = "";
+
+	const randomchar =
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+	// Generate captcha for length of
+	// 5 with random character
+	for (let i = 1; i < 5; i++) {
+		uniquechar += randomchar.charAt(
+			Math.random() * randomchar.length)
+	}
+
+	// Store generated input
+	captcha.innerHTML = uniquechar;
 }
 
-generateCaptcha();
+function printmsg() {
+	const usr_input = document
+		.getElementById("submit").value;
 
-captchaInput.addEventListener("input", function() {
-  if (captchaInput.value === captcha.innerHTML) {
-    // The CAPTCHA is correct.
-  } else {
-    // The CAPTCHA is incorrect.
-  }
-});
+	// Check whether the input is equal
+	// to generated captcha or not
+	if (usr_input == captcha.innerHTML) {
+		let s = document.getElementById("key")
+			.innerHTML = "Matched";
+		generate();
+	}
+	else {
+		let s = document.getElementById("key")
+			.innerHTML = "not Matched";
+		generate();
+	}
+}
