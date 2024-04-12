b119e381bc0625f88a075af401c50711bf0040f8
MOKSHITH Y NANDA <codersy17mc@gmail.com>
2024-04-12 06:07:47 +0530

    Update loginJS.js
    Signed-off-by: MOKSHITH Y NANDA <codersy17mc@gmail.com>

--- Java Script/loginJS.js
+++ Java Script/loginJS.js
@@ -70,7 +70,7 @@ function goto () {
 
 
 
-<script type="text/javascript">
+
         function generateCaptcha() {
             var alpha = new Array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
             var i;
@@ -99,4 +99,4 @@ function goto () {
         function removeSpaces(string) {
             return string.split(' ').join('');
         }
-    </script>
+
