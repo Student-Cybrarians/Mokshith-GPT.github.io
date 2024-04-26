994db656e53eeb9a3d1038622d6b81bab084930c
Mokshith-GPT <codersy17mc@gmail.com>
2024-04-27 01:54:25 +0530

    push to signup of aws

--- HTML/signup.php
+++ HTML/signup.php
@@ -26,48 +26,7 @@ $sql = "CREATE TABLE IF NOT EXISTS users (
 if (mysqli_query($conn, $sql)) {
 echo "Table users created successfully";
 } else {
-echo "Error creating table: " . mysqli_error($conn);
+echo "Error creating table: " . $conn->error;
 }
 
-// Get form data
-$name = $_POST['name'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$username = $_POST['username'];
-$phone = $_POST['phone'];
-
-// Insert form data into users table
-$sql = "INSERT INTO users (name, email, password, username, phone) VALUES ('$name', '$email', '$password', '$username', '$phone')";
-
-if (mysqli_query($conn, $sql)) {
-echo "New record created successfully";
-} else {
-echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-
-// Close database connection
-mysqli_close($conn);
-
-?>
-
-// Embed signup form HTML
-<form action="signup.php" method="post">
-
-    <label for="name">Name:</label>
-    <input type="text" id="name" name="name"><br><br>
-
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email"><br><br>
-
-    <label for="password">Password:</label>
-    <input type="password" id="password" name="password"><br><br>
-
-    <label for="username">Username:</label>
-    <input type="text" id="username" name="username"><br><br>
-
-    <label for="phone">Phone:</label>
-    <input type="text" id="phone" name="phone"><br><br>
-
-    <input type="submit" value="Submit">
-
-</form>
+$conn->close();
