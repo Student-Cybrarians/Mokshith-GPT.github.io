b7bf3dd3be0931b8e9ba4dae61c12af6ad03e951
MOKSHITH Y NANDA <codersy17mc@gmail.com>
2024-05-13 06:10:06 +0530

    Update signupJS.js

--- Java Script/signupJS.js
+++ Java Script/signupJS.js
@@ -105,11 +105,7 @@ function validForm () {
         wrong();
     }
 }
-    // Check if reCAPTCHA is solved
-    if (!grecaptcha.getResponse()) {
-        alert('Please complete the reCAPTCHA challenge.');
-        return false;
-    }
+
 function goto () {
     document.getElementById( "signupbtn" ).style.borderStyle = "inset";
     window.location.replace( "home.html" );
