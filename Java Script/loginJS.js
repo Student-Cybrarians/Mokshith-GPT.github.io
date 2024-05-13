1bfde105b4bb4eff38570c17afc66552edf95110
MOKSHITH Y NANDA <codersy17mc@gmail.com>
2024-05-13 15:14:12 +0530

    Update loginJS.js
    Signed-off-by: MOKSHITH Y NANDA <codersy17mc@gmail.com>

--- Java Script/loginJS.js
+++ Java Script/loginJS.js
@@ -1,73 +1,44 @@
-function load () {
-    wrong();
-    document.getElementById( "accno" ).value = "";
-    document.getElementById( "email" ).value = "";
-}
-function retEmail () {
-    return document.getElementById( "email" ).value;
-}
-function wrong () {
-    document.getElementById( "loginbtn" ).disabled = true;
-    document.getElementById( "accno" ).focus();
-    document.getElementById( "loginbtn" ).style.opacity = "0.5";
-}
-function validForm () {
-    var email = document.getElementById( "email" ).value;
-    var accno = document.getElementById( "accno" ).value;
-    var pass = document.getElementById( "password" ).value;
-    if ( email.length == 0 )
-    {
-        if ( /[0-9]/.test( accno ) == true && ( accno.length != 16 ) )
-        {
-            document.getElementById( "result" ).innerHTML = "Account No. must be of 16 numeric characters.";
-            wrong();
-            return false;
-        }
-        else
-        {
-            if ( pass.length < 6 )
-            {
-                document.getElementById( "result" ).innerHTML = "Password must contain more than 6 charcaters.";
-                wrong();
-                return false;
-            }
-            else if ( pass.length >= 6 )
-            {
-                document.getElementById( "loginbtn" ).disabled = false;
-                document.getElementById( "result" ).innerHTML = "";
-                document.getElementById( "loginbtn" ).style.opacity = "1";
-                return true;
-            }
-        }
-    }
-    else
-    {
-        if ( pass.length < 6 )
-        {
-            document.getElementById( "result" ).innerHTML = "Password must contain more than 6 charcaters.";
-            wrong();
-            return false;
-        }
-        else if ( pass.length >= 6 )
-        {
-            document.getElementById( "loginbtn" ).disabled = false;
-            document.getElementById( "result" ).innerHTML = "";
-            document.getElementById( "loginbtn" ).style.opacity = "1";
-            return true;
-        }
-    }
-}
-    // Check if reCAPTCHA is solved
-    if (!grecaptcha.getResponse()) {
-        alert('Please complete the reCAPTCHA challenge.');
-        return false;
-    }
-function goto () {
-    document.getElementById( "loginbtn" ).style.borderStyle = "inset";
-    window.location.replace( "home.html" );
-    return true;
-}
 
+        function load() {
+            wrong();
+            document.getElementById("email").focus();
+        }
+
+        function wrong() {
+            document.getElementById("loginbtn").disabled = true;
+            document.getElementById("loginbtn").style.opacity = "0.5";
+            document.getElementById("accno").focus();
+        }
+
+        function validForm() {
+            var email = document.getElementById("email").value;
+            var accno = document.getElementById("accno").value;
+            var pass = document.getElementById("password").value;
+
+            if (email.length == 0) {
+                if (/[0-9]/.test(accno) && accno.length == 16) {
+                    if (pass.length >= 6) {
+                        return true;
+                    } else {
+                        document.getElementById("result").innerHTML = "Password must contain more than 6 characters.";
+                        wrong();
+                        return false;
+                    }
+                } else {
+                    document.getElementById("result").innerHTML = "Account No. must be of 16 numeric characters.";
+                    wrong();
+                    return false;
+                }
+            } else {
+                if (pass.length >= 6) {
+                    return true;
+                } else {
+                    document.getElementById("result").innerHTML = "Password must contain more than 6 characters.";
+                    wrong();
+                    return false;
+                }
+            }
+        }
     // Check if reCAPTCHA is solved
     if (!grecaptcha.getResponse()) {
         alert('Please complete the reCAPTCHA challenge.');
